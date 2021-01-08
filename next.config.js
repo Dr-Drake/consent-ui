1ded0b463acd2e844a720bf9d9f7cba0e3c5c98c
Ikem Ezechukwu <40820909+Dr-Drake@users.noreply.github.com>
2021-01-08 14:14:13 +0000

    Delete next.config.js
    Not supposed to be here, lol.

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    env:{
-        LOGIN_API: "http://localhost:1004/authenticate",
-        API_KEY: "1T7CfvSdBIlU4mXnyN6gpCq3RxQFvVYQ`%$",
-        APPLICATION_API: "http://localhost:1005/applications",
-        REFRESH_API: "http://localhost:1004/refresh_token",
-        LOGOUT_API: "http://localhost:1004/logout"
-    }
-}
